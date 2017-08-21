4af1a60fae40d3c13eaca79e265d30241b2de52b
pradeepsaba <30779936+pradeepsaba@users.noreply.github.com>
2017-08-22 01:31:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,114 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+var articles={
+    
+    'article-one':{
+    title:'Article-one Pradeep Saba',
+    heading:'article-one',
+    date:'22-08-2017',
+    content: `<p>
+             This is article-one This is article-one This is article-one This is article-one This is article-one
+             This is article-oneThis is article-oneThis is article-oneThis is article-oneThis is article-one
+        </p> 
+        <p>
+            This is content This is article-one This is article-one This is article-one This is article-one
+            This is article-one This is article-one This is article-one This is article-one
+            This is article-oneThis is article-one
+        </p>
+        <p>
+            This is content This is article-one This is article-one This is article-one This is article-one
+            This is article-one This is article-one This is article-one This is article-one
+            This is article-oneThis is article-one
+        </p>`
+        
+    },
+    'article-two':{
+    title:'Article-Two Pradeep Saba',
+    heading:'article-two',
+    date:'22-08-2017',
+    content: `<p>
+             This is article-one This is article-one This is article-one This is article-one This is article-one
+             This is article-oneThis is article-oneThis is article-oneThis is article-oneThis is article-one
+        </p> 
+        <p>
+            This is content This is article-one This is article-one This is article-one This is article-one
+            This is article-one This is article-one This is article-one This is article-one
+            This is article-oneThis is article-one
+        </p>
+        <p>
+            This is content This is article-one This is article-one This is article-one This is article-one
+            This is article-one This is article-one This is article-one This is article-one
+            This is article-oneThis is article-one
+        </p>`
+        
+    },
+    'article-three':{
+    title:'Article-Three Pradeep Saba',
+    heading:'article-Three',
+    date:'22-08-2017',
+    content: `<p>
+             This is article-one This is article-one This is article-one This is article-one This is article-one
+             This is article-oneThis is article-oneThis is article-oneThis is article-oneThis is article-one
+        </p> 
+        <p>
+            This is content This is article-one This is article-one This is article-one This is article-one
+            This is article-one This is article-one This is article-one This is article-one
+            This is article-oneThis is article-one
+        </p>
+        <p>
+            This is content This is article-one This is article-one This is article-one This is article-one
+            This is article-one This is article-one This is article-one This is article-one
+            This is article-oneThis is article-one
+        </p>`
+        
+    }
+    
+};
+
+function createTemplate(data)
+{
+    
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemplate=`
+    <HTML>
+<head>
+    <TITLE>
+            ${title}
+    </TITLE>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+    <body>
+        <div>
+           <a href="/">HOME</a>
+        </div> 
+         <HR/>   
+        <h3>
+           ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div class="container">
+           ${content}
+        </div>
+    </body>
+</HTML>`
+;
+return htmlTemplate;
+}
+
+
+app.get('/:articlename', function (req, res) {
+    var articlename=req.params.articlename;
+  res.send(createTemplate(articles[articlename]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send("this is article-two");
-});
 
-app.get('/articel-three', function (req, res) {
-  res.send("this is article-three");
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
