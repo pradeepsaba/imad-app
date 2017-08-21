9100e3c74fe772d0c6eb660b08db99e521c69018
pradeepsaba <30779936+pradeepsaba@users.noreply.github.com>
2017-08-21 23:35:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send("this is article-one");
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send("this is article-two");
 });
 
 app.get('/articel-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send("this is article-three");
 });
 
 app.get('/ui/style.css', function (req, res) {
