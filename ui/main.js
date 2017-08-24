5093fb9241003daaa805f83e6b7ab501ce24702a
pradeepsaba <30779936+pradeepsaba@users.noreply.github.com>
2017-08-25 01:23:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick = function() {
-    console.log("button clciked")
-counter= counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
-}
+    var request = new XMlHttpRequest();
+    
+    request.onreadystatechange=function(){
+     
+     if (request.readystate === XMlHttpRequest.Done) {
+         
+         if(request.status === 200) {
+            var counter = request.responsetext;
+            var span = document.getElementById('count');
+            span.innerHTML=counter.toString();
+         }
+         
+     }
+
+    };
+    
+    request.open('GET','http://http://pradeepsaba.imad.hasura-app.io/counter',true)
+    request.send(null);
+    
+};
