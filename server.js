5a8f09211bd58302f7ebcde5dc8e7751083c60ea
pradeepsaba <30779936+pradeepsaba@users.noreply.github.com>
2017-08-25 00:23:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'harin.jpg'));
 });
 
 app.get('/ui/main.js', function (req, res) {
